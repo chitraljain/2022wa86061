c1ac7b1b5b03e14e0f0787eabf3cf93801520c1a
chitraljain <115804525+chitraljain@users.noreply.github.com>
2026-09-19 16:45:34 +0530

    Update App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello Jenkins CI");
+        System.out.println("Hello Jenkins CI- Polling Test");
     }
 }
